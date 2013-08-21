00abfcae16f152f1602db49d1ef4f095c95d4045
Haralan Dobrev <hkdobrev@gmail.com>
2013-08-22 00:40:33 +0300

    Added unit tests for the Exceptions

---
+++ tests/tests/Exception/SourceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use OpenBuildings\Monetary\Exception_Source as Exception_Source;
+
+/**
+ * Test OpenBuildings\Monetary\Exception class.
+ * @author Haralan Dobrev <hdobrev@despark.com>
+ * @copyright (c) 2013 OpenBuildings Inc.
+ * @license http://spdx.org/licenses/BSD-3-Clause
+ */
+class Exception_SourceTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @covers OpenBuildings\Monetary\Exception_Source::__construct
+	 */
+	public function test_constructor()
+	{
+		$exception = new Exception_Source('some message :alabala :source_name', 'qwerty', array(
+			':alabala' => 'abcde'
+		));
+
+		$this->assertSame($exception->getMessage(), 'some message abcde qwerty');
+		$this->assertSame($exception->source_name, 'qwerty');
+	}
+}
---
+++ tests/tests/ExceptionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use OpenBuildings\Monetary\Exception as Exception;
+
+/**
+ * Test OpenBuildings\Monetary\Exception class.
+ * @author Haralan Dobrev <hdobrev@despark.com>
+ * @copyright (c) 2013 OpenBuildings Inc.
+ * @license http://spdx.org/licenses/BSD-3-Clause
+ */
+class ExceptionTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @covers OpenBuildings\Monetary\Exception::__construct
+	 */
+	public function test_constructor()
+	{
+		$exception = new Exception('some message :alabala', array(
+			':alabala' => 'abcde'
+		));
+
+		$this->assertSame($exception->getMessage(), 'some message abcde');
+	}
+}
